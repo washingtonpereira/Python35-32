7c65a9c8c269ad7879458afb913afcaf7e391c6a
Washington <slash.o.teobaldo@gmail.com>
2017-04-12 21:39:56 -0300

    Calculadora ADS!

---
+++ Calculadora.py
@@ -0,0 +1,24 @@
+#CALCULADORA ADS!!!
+#TIPO DE OPERACAO (+,-,*,/)
+
+n1 = float(input("digite o primeiro número:"))
+n2 = float(input("digite o primeiro segundo:"))
+operacao =input("informe a operação:")
+ 
+
+if  operacao== "+":
+   print(n1+n2)
+
+elif operacao== "-": 
+    print(n1-n2)
+elif operacao== "*": 
+     print(n1*n2 )
+elif operacao == '/':
+     print(n1/n2)
+else:
+    print('opcao invalida')
+
+
+
+      
+
